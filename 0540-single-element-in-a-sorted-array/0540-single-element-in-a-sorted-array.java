6d1ee63146e502f2aa12bfd6f92c2aa67b13db90
Siddhant Ganesh Abhang <142685375+SiddhantGAbhang@users.noreply.github.com>
2026-07-08 15:02:04 +0530

    Time: 0 ms (100%), Space: 53 MB (21.16%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (mid % 2 == 1) {
+                mid--;
+            }
+
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+               
+                high = mid;
+            }
+        }
+
+        return nums[low];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
