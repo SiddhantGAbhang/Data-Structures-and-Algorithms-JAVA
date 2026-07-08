514c83909725a231f660cb261c3c5fffa71ecb0d
Siddhant Ganesh Abhang <142685375+SiddhantGAbhang@users.noreply.github.com>
2026-07-08 14:54:42 +0530

    Time: 1 ms (100%), Space: 47.1 MB (76.31%) - LeetHub - Time: O(n), Space: O(n)

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,6 +1,37 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        
+      int n = nums.length;
+
+        // Case 1: all non-negative
+        if (nums[0] >= 0) {
+            for (int i = 0; i < n; i++)
+                nums[i] = nums[i] * nums[i];
+            return nums;
+        }
+
+        // Case 2: all non-positive
+        if (nums[n - 1] <= 0) {
+            int[] res = new int[n];
+            for (int i = n - 1, j = 0; i >= 0; i--, j++)
+                res[j] = nums[i] * nums[i];
+            return res;
+        }
+
+        // Case 3: mixed → use two pointer
+        int[] res = new int[n];
+        int l = 0, r = n - 1, k = n - 1;
+
+        while (l <= r) {
+            int a = nums[l], b = nums[r];
+            if (a * a > b * b) {
+                res[k--] = a * a;
+                l++;
+            } else {
+                res[k--] = b * b;
+                r--;
+            }
+        }
+        return res;
     }
 }
 
